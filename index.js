a2516580195efcf4f18f248c81c6068fd40917a2
Anura_Nishantha <31471714+AKNishantha@users.noreply.github.com>
2020-08-01 14:53:57 +0530

    fix port to deploy

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use('/azurestudent',require('./routes/usersR'));
 
 //port cjhangin
 
-const PORT = proces.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>{
    console.log("Server listning on port", PORT);
 });
